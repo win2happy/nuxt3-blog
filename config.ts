2b30a72598f7d6bfc8d506581be9b1d0e0ca7aa7
win2happy <win.lee.svip@gmail.com>
2025-12-25 17:50:41 +0800

    #1 初始化项目

--- config.ts
+++ config.ts
@@ -5,15 +5,15 @@ export default {
 
   /** -------------------------------------------------以下可选修改----------------------------------------------------- */
 
-  title: "<your-blog-title>", // 网站标题
-  nickName: "<your-nickname>", // 昵称
-  domain: "https://<your-blog-domain>", // rss域名
-  SEO_title: " - <your-blog-title>", // 搜索引擎显示的标题
-  SEO_keywords: "<your-keywords>", // keywords meta header
-  MSClarityId: "", // Microsoft的Clarity统计，https://clarity.microsoft.com/
-  CloudflareAnalyze: "", // cloudflare的统计，https://developers.cloudflare.com/analytics/web-analytics
-  CommentRepoId: "", // 评论系统，参考 https://github.com/yunyuyuan/nuxt3-blog/wiki/2.3-%E8%AF%84%E8%AE%BA%E7%B3%BB%E7%BB%9F
-  CommentDiscussionCategoryId: "", // 评论系统
+  title: "Imuer视界", // 网站标题
+  nickName: "IM", // 昵称
+  domain: "https://blog.pnt.pp.ua", // rss域名
+  SEO_title: " - IM blog", // 搜索引擎显示的标题
+  SEO_keywords: "IM,imuer's blog,Imuer视界,Imuer视界的博客", // keywords meta header
+  MSClarityId: "uqyzrv8y66", // Microsoft的Clarity统计，https://clarity.microsoft.com/
+  CloudflareAnalyze: "1c42957b8103aa01e99ea05293952604", // cloudflare的统计，https://developers.cloudflare.com/analytics/web-analytics
+  CommentRepoId: "R_kgDOQuotxw", // 评论系统，参考 https://github.com/yunyuyuan/nuxt3-blog/wiki/2.3-%E8%AF%84%E8%AE%BA%E7%B3%BB%E7%BB%9F
+  CommentDiscussionCategoryId: "DIC_kwDOQuotx84C0Oko", // 评论系统
 
   database: { // 参考 https://github.com/yunyuyuan/nuxt3-blog/wiki/2.4-%E6%B5%8F%E8%A7%88%E9%87%8F%E7%BB%9F%E8%AE%A1
     initialVisitors: 1, // 如果设置成10000，那么发一篇文章立马就有10000个浏览量！
@@ -22,14 +22,17 @@ export default {
   algoliaSearch: { // 参考 https://github.com/yunyuyuan/nuxt3-blog/wiki/2.5-%E5%85%A8%E7%AB%99%E6%90%9C%E7%B4%A2
     appId: "",
     searchKey: "",
-    indexName: ""
+    indexName: "nuxt3-blog"
   },
   themeColor: ["cyan", "sky", "teal", "emerald", "purple", "indigo", "fuchsia", "orange", "amber"], // 主题色，如果有多个颜色，则随机使用，参考 https://tailwindcss.com/docs/colors
   themeColorDark: "neutral", // 主题色(夜间模式)
   defaultLang: "zh", // default language, "zh" and "en" are supported currently
   about: [
-    "Hello there",
-    "Welcome to my new blog"
+    "幽深宇宙已岁逾百亿，惟闪烁星光点缀生机",
+    "我常仰望浩瀚天际，思念在同一颗星球的你",
+    "想，那转瞬的迷人流星，也许就是你的回眸",
+    "光坠之地，吾之忧祈",
+    "——2021.12.4"
   ],
 
   /** -------------------------------------------------注意----------------------------------------------------- */
